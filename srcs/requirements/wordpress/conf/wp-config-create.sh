7cf2ac28ba5a2c5209510ca9f4aec0cdeb0436d7
mlecherb <mlecherb@debian>
2023-02-17 00:54:42 +0100

    Wordpress fixed

--- srcs/requirements/wordpress/conf/wp-config-create.sh
+++ srcs/requirements/wordpress/conf/wp-config-create.sh
@@ -10,7 +10,17 @@ define( 'DB_CHARSET', 'utf8' );
 define( 'DB_COLLATE', '' );
 define('FS_METHOD','direct');
 \$table_prefix = 'wp_';
-define( 'WP_DEBUG', false );
+define( 'WP_DEBUG', true ); // Or false
+if ( WP_DEBUG ) {
+ define( 'WP_DEBUG_LOG', true );
+ define( 'WP_DEBUG_DISPLAY', false );
+ @ini_set( 'display_errors', 0 );
+}
+define('CONCATENATE_SCRIPTS', false);
+define('FORCE_SSL_LOGIN', true);
+define('FORCE_SSL_ADMIN', true);
+define( 'CONCATENATE_SCRIPTS', false );
+define( 'SCRIPT_DEBUG', true );
 if ( ! defined( 'ABSPATH' ) ) {
 define( 'ABSPATH', __DIR__ . '/' );}
 define( 'WP_REDIS_HOST', 'redis' );
